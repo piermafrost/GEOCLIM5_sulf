df7e10bf0b446d101c08d7edbbcca25d16ef59ea
Pierre Maffre <maffre@cheyenne2.cheyenne.ucar.edu>
2021-04-02 10:43:51 -0600

    modify executable name

--- job/run_geoclim.sh
+++ job/run_geoclim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -A UCBK0027
 #PBS -N G-car
-#PBS -o geoclim-car.log
+#PBS -o geoclim.log
 #PBS -j oe
 #PBS -q regular
 #PBS -l walltime=12:00:00
@@ -12,9 +12,8 @@ export TMPDIR=/glade/scratch/$USER/temp
 mkdir -p $TMPDIR
 
 ### Run OpenMP program
-cd ../executable/
-./geoclim-car.exe 0 1 3 0 0
+./geoclim.exe 0 1 3 0 0
 
 # Recursive resubmission
-cd ../job/
 ./submit_chain.sh CONTINUE_RUN
+
--- job/run_geoclim_basic.sh
+++ job/run_geoclim_basic.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-../source/geoclim.exe 0 1 3 0 0
+
+# Run geoclim model
+./geoclim.exe 0 1 3 0 0
+
+# Recursive resubmission
 ./submit_chain.sh CONTINUE_RUN
+
