0b96fa50b8b39ac7439b77b2a119260929a001cd
piermafrost <pierre.maffre@normalesup.org>
2021-04-05 15:03:24 -0700

    add extra O2 feedback (commented)

--- source/Phydrotherm.f
+++ source/Phydrotherm.f
@@ -7,6 +7,9 @@
     do i=1,nbasin
         fhydP(i)=0.325d+10*var(3,i)/1.5d-3*clo*phosss* &
                  indice_deep(i)
+        !! add O2 feedback
+        !fhydP(i)=1.505d+10*var(3,i)/1.5d-3*var(11,i)*clo*phosss * indice_deep(i)
+        !!              PO4 ^^^^^^^^     O2 ^^^^^^^^^
     enddo
     return
     end
