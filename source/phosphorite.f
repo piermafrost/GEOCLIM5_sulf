737c1387f8afdd7030e17caa6303879e8bcb1124
piermafrost <pierre.maffre@normalesup.org>
2021-04-05 16:25:28 -0700

    reduce strength of extra O2 feedback (commented)

--- source/phosphorite.f
+++ source/phosphorite.f
@@ -7,8 +7,8 @@
     do i=1,nbasin
         fphos(i)=akphos*var(3,i)*clo*phosss*indice_deep(i)
         !!add O2 feedback
-        !fphos(i)=akphos*var(3,i)*var(11,i)/2.161d-1*clo*phosss*indice_deep(i)
-        !!          PO4 _^^^^^^^^ ^^^^^^^^^_ O2
+        !fphos(i)=akphos*var(3,i)*(var(11,i)/0.2163)**0.25 * clo*phosss*indice_deep(i)
+        !!          PO4 _^^^^^^^^  ^^^^^^^^^_ O2
     enddo
     return
     end
