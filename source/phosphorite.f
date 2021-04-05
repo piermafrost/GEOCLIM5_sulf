14fcdcd07e7aefe35f9448aa2d4660e56c1a4d2b
piermafrost <pierre.maffre@normalesup.org>
2021-04-05 15:36:20 -0700

    add extra O2 feedback (commented)

--- source/phosphorite.f
+++ source/phosphorite.f
@@ -6,6 +6,9 @@
 
     do i=1,nbasin
         fphos(i)=akphos*var(3,i)*clo*phosss*indice_deep(i)
+        !!add O2 feedback
+        !fphos(i)=akphos*var(3,i)*var(11,i)/2.161d-1*clo*phosss*indice_deep(i)
+        !!          PO4 _^^^^^^^^ ^^^^^^^^^_ O2
     enddo
     return
     end
