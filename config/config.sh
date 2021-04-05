df2a08a501e22346545e6fc529a969af1d84e59f
Pierre Maffre <maffre@cheyenne1.cheyenne.ucar.edu>
2021-04-05 15:21:20 -0600

    missing extension

--- config/config.sh
+++ config/config.sh
@@ -4,7 +4,7 @@
 # 1 input argument: name of configuration 
 
 cp -f templates/IO_CONDITIONS.$1 IO_CONDITIONS
-cp -f templates/cond_p20.${1}.dat cond_p20
+cp -f templates/cond_p20.${1}.dat cond_p20.dat
 cp -f templates/GCM_input_conditions.$1 GCM_input_conditions
 cd ../source/
 cp -f templates/constante.f90.$1 constante.f90
