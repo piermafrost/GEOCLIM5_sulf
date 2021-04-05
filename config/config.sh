391c77865f0f2c0b1b14c31e0e1f4e9d215ec9af
Pierre Maffre <maffre@cheyenne1.cheyenne.ucar.edu>
2021-04-05 15:16:23 -0600

    automatic configuration setup from templates

---
+++ config/config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set GEOCLIM configuration, provided that the files exist in templates
+# 1 input argument: name of configuration 
+
+cp -f templates/IO_CONDITIONS.$1 IO_CONDITIONS
+cp -f templates/cond_p20.${1}.dat cond_p20
+cp -f templates/GCM_input_conditions.$1 GCM_input_conditions
+cd ../source/
+cp -f templates/constante.f90.$1 constante.f90
+cp -f templates/shape.inc.$1 shape.inc
+
