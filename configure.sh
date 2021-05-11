bc6568a90c7c9d08711acaab701c95010c883177
piermafrost <pierre.maffre@normalesup.org>
2021-05-11 11:57:42 -1000

    moved and updated

--- configure.sh
+++ configure.sh
@@ -3,6 +3,7 @@
 # Set GEOCLIM configuration, provided that the files exist in templates
 # 1 input argument: name of configuration 
 
+cd config/
 cp -f templates/IO_CONDITIONS.$1 IO_CONDITIONS
 cp -f templates/cond_p20.${1}.dat cond_p20.dat
 cp -f templates/GCM_input_conditions.$1 GCM_input_conditions
