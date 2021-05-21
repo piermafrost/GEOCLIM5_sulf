823b87d50df6504cfc5df8ae819b53202c3bedeb
piermafrost <pierre.maffre@normalesup.org>
2021-05-20 16:53:42 -1000

    adjust Sr negative feedback to get closer to 80mmol/m3

--- source/strontium_ratio.f
+++ source/strontium_ratio.f
@@ -1,6 +1,6 @@
     subroutine strontium_ratio(t)
 !   *****************************
-    use constante, only: rSrCar
+    use constante, only: rSrCar, rSrSil
     implicit none
     include 'combine_foam.inc'
 
@@ -8,7 +8,7 @@
     rsw=weighted_rsw
     do j0=1,nsurface
         j = jbox_surface(j0)
-        rSrdep(j)=rSrCar*var(5,j)/80.d-3
+        rSrdep(j) = (0.72*rSrCar + 0.28*rSrSil)*var(5,j)/80.d-3
     enddo
     return
     end
